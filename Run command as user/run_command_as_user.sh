a3cae5739bad7f109c5f3ee71b4ac8818a8f4e03
Ali Al-Itejawi <ali@al-itejawi.com>
2020-11-23 15:35:29 +0100

    Forgot $ and added verbosity

--- Run command as user/run_command_as_user.sh
+++ Run command as user/run_command_as_user.sh
@@ -4,7 +4,9 @@
 UUID=$(id -u $(logname))
 echo "User $(logname): $UUID"
 
-# Loop through parameters set in Jamf (4 - 11)
+# Set the commands you want to run in Jamf
+
+# Loop through parameters set in Jamf ($4 - $11)
 for Command in "${@:4}"; do
     # Skip empty commands
     [ -z "$Command" ] && continue
