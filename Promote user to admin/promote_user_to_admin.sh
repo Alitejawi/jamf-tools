09856d2cea2389173c506b9af34c954c4b2072c7
Ali Al-Itejawi <ali@al-itejawi.com>
2020-11-25 02:08:08 +0100

    Typo fix

--- Promote user to admin/promote_user_to_admin.sh
+++ Promote user to admin/promote_user_to_admin.sh
@@ -2,7 +2,7 @@
 
 # Convert the logged in user to admin user
 
-# You can use either of the 2 options below, as logname should always returns the correct value
+# You can use either of these 2 options below, as $logname should always returns the correct value
 #loggedInUser=$logname
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
