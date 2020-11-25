c0316b97358204996233c9dc049ede2cac2a5548
Ali Al-Itejawi <ali@al-itejawi.com>
2020-11-25 23:31:41 +0100

    Network accounts

---
+++ Network accounts available/README.md
@@ -0,0 +1,3 @@
+# Check if network accounts are present on the machine
+
+This script loops through dscl for the users and verifies if they are network accounts or not
---
+++ Network accounts available/has_network_account.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NETACCLIST=`dscl . list /Users OriginalNodeName | awk '{print $1}' 2>/dev/null`
+
+if [ "$NETACCLIST" == "" ]; then
+        echo "<result>No Network Accounts</result>"
+else
+        echo "<result>$NETACCLIST</result>"
+fi
+exit 0
