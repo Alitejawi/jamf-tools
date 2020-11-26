18aa0ae6fd457c2702beb72327a46750ebd19bc3
Ali Al-Itejawi <ali@al-itejawi.com>
2020-11-26 14:47:32 +0100

    Added secureToken status

---
+++ Scripts/who_has_securetoken.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AllUsers=$(dscl . list /Users | grep -v _)
+
+for EachUser in $AllUsers; do
+
+TokenValue=$(sysadminctl -secureTokenStatus $EachUser 2>&1)
+    echo "Checking $EachUser"
+
+	if [[ $TokenValue = *"ENABLED"* ]]; then
+        SecureTokenUsers+=($EachUser)
+	fi
+done
+
+    if [[ -z "${SecureTokenUsers[@]}" ]]; then
+        echo "<result>No Users</result>"
+	        else
+                printf '%s,' '<result>'"${SecureTokenUsers[@]}"'</result>'
+    fi
+
+exit
