3a8314bb77467f350b6979420351ca5b6be60821
Ali Al-Itejawi <ali@al-itejawi.com>
2020-12-02 16:28:12 +0100

    Find min and max values in list of integers

---
+++ Scripts/find_min_and_max_without_builtin_functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+__author__ = "Ali Al-Itejawi"
+__version__ = "1.0"
+__email__ = "ali.alitejawi@booking.com"
+
+
+__description__ = "This script will find the minimum value and maximum value of a list of integers without using the built-in functions"
+
+data = [3, 4, 6, 8, 9, -1, -4, 0, 7]
+
+
+max = min = data[0]
+for x in data:
+    if x < min:
+        min = x
+    elif x > max:
+        max = x
+
+print('Minimum =', min)
+print('Maximum =', max)
