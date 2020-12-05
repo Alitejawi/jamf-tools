a78b484a922f5764b29ac9b02ae3509a2fb9093a
Ali Al-Itejawi <ali@al-itejawi.com>
2020-12-05 23:11:40 +0100

    Added account discovery EA

--- Extension Attributes/local_or_network_account.sh
+++ Extension Attributes/local_or_network_account.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#loggedinuser=$logname
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
+
+# Check OriginalNodeName attribute to determine domain user status.
+if /usr/bin/dscl . -read "/Users/$loggedInUser" OriginalNodeName 2>&1 | /usr/bin/grep -q "No such key"; then
+  accountType="Local User"
+else
+  accountType="Domain User"
+fi
+
+# Report result.
+echo "<result>$accountType</result>"
+
+exit 0
