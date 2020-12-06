187b57633bbaedaca6e280b96d70144d4a561020
Ali Al-Itejawi <ali@al-itejawi.com>
2020-12-06 21:17:18 +0100

    Added options to promote user to admin script

--- Scripts/admin_promote_user_to_admin.sh
+++ Scripts/admin_promote_user_to_admin.sh
@@ -4,6 +4,20 @@
 
 # You can use either of these 2 options below, as $logname should always returns the correct value
 #loggedInUser=$logname
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
 
 /usr/sbin/dseditgroup -o edit -a $loggedInUser -t user admin
+echo "Granted admin privileges to $loggedInUser."
+
+
+
+
+# Grant admin privileges to $loggedInUser.
+#if /usr/bin/dscl . -read "/groups/admin" GroupMembership | /usr/bin/grep -q "$loggedInUser"; then
+#  echo "$loggedInUser already has admin privileges, no action required."
+#else
+#  /usr/bin/dscl . -append "/groups/admin" GroupMembership "$loggedInUser"
+#  echo "Granted admin privileges to $loggedInUser."
+#fi
+
+#exit 0
