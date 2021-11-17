8d062bfa73f7eab3ee2f4f9ca49dbfcb67c0fe01
Ali Al-Itejawi <ali@al-itejawi.com>
2021-11-17 21:01:12 +0100

    Corrected result for computer serial

--- Extension Attributes/get_computer_serial_number.sh
+++ Extension Attributes/get_computer_serial_number.sh
@@ -3,4 +3,4 @@
 # Use this in an extension attribute
 serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 
-echo "<result>serial_number</result>"
+echo "<result>$serial_number</result>"
