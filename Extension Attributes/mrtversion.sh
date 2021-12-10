672c1c1c235ead9a997e449bf714085d386dad1a
Ali Al-Itejawi <ali@al-itejawi.com>
2021-12-10 21:34:28 +0100

    Removed duplicate

--- Extension Attributes/mrtversion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Authenticated Root MUST be enabled
-mrtstatus=$(/usr/bin/defaults read /Library/Apple/System/Library/CoreServices/MRT.app/Contents/Info.plist CFBundleShortVersionString)
-
-# Report result, use "<result>$defaulttype</result>" to have Jamf report only the result
-echo "<result>$mrtstatus</result>"
