a945bcc68f2ce36fb55595fc4a744a8f923ccc7f
Ali Al-Itejawi <ali@al-itejawi.com>
2021-12-11 20:36:45 +0100

    Removed obsolete items

--- EA/list_all_filevault_users.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Variables
-fdeList=$(/usr/bin/fdesetup list | /usr/bin/awk -F',' '{print $1}')
-
-# Report result, use "<result>$defaulttype</result>" to have Jamf report only the result
-echo "<result>$fdeList</result>"
--- EA/report_hostname.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "<result>$(/bin/hostname 2>&1)</result>"
