077b7d797072affa2f7ae22e285e881bd16af3dc
Ali Al-Itejawi <ali@al-itejawi.com>
2021-12-22 23:52:59 +0100

    Added to find app arch

--- Scripts/app_architecture.sh
+++ Scripts/app_architecture.sh
@@ -7,3 +7,9 @@ lipo -archs /Applications/WhatsApp.app/Contents/MacOS/WhatsApp
 # arm64
 # or
 # x86_64 arm64
+
+file /Applications/WhatsApp.app/Contents/MacOS/WhatsApp
+# Results will look like
+# /Applications/Keka.app/Contents/MacOS/Keka: Mach-O universal binary with 2 architectures: [x86_64:Mach-O 64-bit executable x86_64] [arm64:Mach-O 64-bit executable arm64]
+# /Applications/Keka.app/Contents/MacOS/Keka (for architecture x86_64):	Mach-O 64-bit executable x86_64
+# /Applications/Keka.app/Contents/MacOS/Keka (for architecture arm64):	Mach-O 64-bit executable arm64
