5a7f5e6320ac18e436114d28052f2da014bc030c
Ali Al-Itejawi <ali@al-itejawi.com>
2022-01-04 23:01:33 +0100

    Absolutes

--- Scripts/block_updates.sh
+++ Scripts/block_updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ignore the title of the software update, specify this in parameter $4 in Jamf
-softwareupdate --ignore "$4"
+/usr/sbin/softwareupdate --ignore "$4"
 
 # Remove the red notification dot above the System Preferences (must be run as user)
-sudo -u logname defaults write com.apple.systempreferences AttentionPrefBundleIDs -int 0
+sudo -u logname /usr/bin/defaults write com.apple.systempreferences AttentionPrefBundleIDs -int 0
