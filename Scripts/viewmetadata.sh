45c82ba60a744ef5284c09f93aa2c65a98d66c2e
Ali Al-Itejawi <ali@al-itejawi.com>
2022-01-04 22:58:23 +0100

    View metadata

---
+++ Scripts/viewmetadata.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+mdls ~/Downloads/TorBrowser-8.0.4-osx64_en-US.dmg
+
+#kMDItemContentCreationDate         = 2019-01-01 00:00:00 +0000
+#kMDItemContentCreationDate_Ranking = 2019-01-01 00:00:00 +0000
+#kMDItemContentModificationDate     = 2019-01-01 00:00:00 +0000
+#kMDItemContentType                 = "com.apple.disk-image-udif"
+#kMDItemContentTypeTree             = (
+#    "public.archive",
+#    "public.item",
+#    "public.data",
+#    "public.disk-image",
+#    "com.apple.disk-image",
+#    "com.apple.disk-image-udif"
+#)
+#kMDItemDateAdded                   = 2019-01-01 00:00:00 +0000
+#kMDItemDateAdded_Ranking           = 2019-01-01 00:00:00 +0000
+#kMDItemDisplayName                 = "TorBrowser-8.0.4-osx64_en-US.dmg"
+#kMDItemFSContentChangeDate         = 2019-01-01 00:00:00 +0000
+#kMDItemFSCreationDate              = 2019-01-01 00:00:00 +0000
+#kMDItemFSCreatorCode               = ""
+#kMDItemFSFinderFlags               = 0
+#kMDItemFSHasCustomIcon             = (null)
+#kMDItemFSInvisible                 = 0
+#kMDItemFSIsExtensionHidden         = 0
+#kMDItemFSIsStationery              = (null)
+#kMDItemFSLabel                     = 0
+#kMDItemFSName                      = "TorBrowser-8.0.4-osx64_en-US.dmg"
+#kMDItemFSNodeCount                 = (null)
+#kMDItemFSOwnerGroupID              = 5000
+#kMDItemFSOwnerUserID               = 501
+#kMDItemFSSize                      = 65840402
+#kMDItemFSTypeCode                  = ""
+#kMDItemInterestingDate_Ranking     = 2019-01-01 00:00:00 +0000
+#kMDItemKind                        = "Disk Image"
+#kMDItemWhereFroms                  = (
+#    "https://dist.torproject.org/torbrowser/8.0.4/TorBrowser-8.0.4-osx64_en-US.dmg",
+#    "https://www.torproject.org/projects/torbrowser.html.en"
+#)kMDItemContentCreationDate         = 2019-01-01 00:00:00 +0000
+#kMDItemContentCreationDate_Ranking = 2019-01-01 00:00:00 +0000
+#kMDItemContentModificationDate     = 2019-01-01 00:00:00 +0000
+#kMDItemContentType                 = "com.apple.disk-image-udif"
+#kMDItemContentTypeTree             = (
+#    "public.archive",
+#    "public.item",
+#    "public.data",
+#    "public.disk-image",
+#    "com.apple.disk-image",
+#    "com.apple.disk-image-udif"
+#)
+#kMDItemDateAdded                   = 2019-01-01 00:00:00 +0000
+#kMDItemDateAdded_Ranking           = 2019-01-01 00:00:00 +0000
+#kMDItemDisplayName                 = "TorBrowser-8.0.4-osx64_en-US.dmg"
+#kMDItemFSContentChangeDate         = 2019-01-01 00:00:00 +0000
+#kMDItemFSCreationDate              = 2019-01-01 00:00:00 +0000
+#kMDItemFSCreatorCode               = ""
+#kMDItemFSFinderFlags               = 0
+#kMDItemFSHasCustomIcon             = (null)
+#kMDItemFSInvisible                 = 0
+#kMDItemFSIsExtensionHidden         = 0
+#kMDItemFSIsStationery              = (null)
+#kMDItemFSLabel                     = 0
+#kMDItemFSName                      = "TorBrowser-8.0.4-osx64_en-US.dmg"
+#kMDItemFSNodeCount                 = (null)
+#kMDItemFSOwnerGroupID              = 5000
+#kMDItemFSOwnerUserID               = 501
+#kMDItemFSSize                      = 65840402
+#kMDItemFSTypeCode                  = ""
+#kMDItemInterestingDate_Ranking     = 2019-01-01 00:00:00 +0000
+#kMDItemKind                        = "Disk Image"
+#kMDItemWhereFroms                  = (
+#    "https://dist.torproject.org/torbrowser/8.0.4/TorBrowser-8.0.4-osx64_en-US.dmg",
+#    "https://www.torproject.org/projects/torbrowser.html.en"
+#)
