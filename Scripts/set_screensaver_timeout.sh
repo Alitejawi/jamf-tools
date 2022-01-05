d655f4348a6d90a09b23994a7302ca79556285c2
Ali Al-Itejawi <ali@al-itejawi.com>
2022-01-05 21:42:25 +0100

    Corrected screensaver timeout

--- Scripts/set_screensaver_timeout.sh
+++ Scripts/set_screensaver_timeout.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Get desired setting with default to 300
-[ -z $4 ] && DESIREDTIME=300 || DESIREDTIME=$4
+[ -z "$4" ] && DESIREDTIME=300 || DESIREDTIME=$4
 
 # Check current settings
-CURRENTTIME=$(/usr/bin/sudo -u $(logname) defaults -currentHost read com.apple.screensaver idleTime)
+CURRENTTIME=$(/usr/bin/sudo -u "$(logname)" defaults -currentHost read com.apple.screensaver idleTime)
 [ $? -ne 0 ] && CURRENTTIME=1000000
 [ -z $CURRENTTIME ] && CURRENTTIME=2000000
 [ $CURRENTTIME -eq 0 ] && CURRENTTIME=3000000
 
 if [ $CURRENTTIME -eq 1000000 ]; then
-    /usr/bin/sudo -u $(logname) defaults -currentHost write com.apple.screensaver idleTime -int $DESIREDTIME
-    echo "Screensaver: Error, Presummed not set, Set first value to  $DESIREDTIME"
+    /usr/bin/sudo -u "$(logname)" defaults -currentHost write com.apple.screensaver idleTime -int "$DESIREDTIME"
+    echo "Screensaver: Error, presumably not set, Set first value to $DESIREDTIME"
     
     # Restart the cfpresd to update the preference
 	sudo killall cfprefsd
@@ -20,8 +20,8 @@ if [ $CURRENTTIME -eq 1000000 ]; then
 fi
 
 if [ $CURRENTTIME -eq 2000000 ]; then
-    /usr/bin/sudo -u $(logname) defaults -currentHost write com.apple.screensaver idleTime -int $DESIREDTIME
-    echo "Screensaver: Unexplained erorr, Set first value to $DESIREDTIME"
+    /usr/bin/sudo -u "$(logname)" defaults -currentHost write com.apple.screensaver idleTime -int "$DESIREDTIME"
+    echo "Screensaver: Unexplained error, Set first value to $DESIREDTIME"
     
     # Restart the cfpresd to update the preference
 	sudo killall cfprefsd
@@ -30,7 +30,7 @@ if [ $CURRENTTIME -eq 2000000 ]; then
 fi
 
 if [ $CURRENTTIME -eq 3000000 ]; then
-    /usr/bin/sudo -u $(logname) defaults -currentHost write com.apple.screensaver idleTime -int $DESIREDTIME
+    /usr/bin/sudo -u "$(logname)" defaults -currentHost write com.apple.screensaver idleTime -int "$DESIREDTIME"
     echo "Screensaver: Set to NEVER, Set value to $DESIREDTIME"
     
     # Restart the cfpresd to update the preference
@@ -39,9 +39,9 @@ if [ $CURRENTTIME -eq 3000000 ]; then
     exit 0
 fi
 
-if [ $CURRENTTIME -gt $DESIREDTIME ]; then
+if [ $CURRENTTIME -gt "$DESIREDTIME" ]; then
     # Write the new value
-	/usr/bin/sudo -u $(logname) defaults -currentHost write com.apple.screensaver idleTime -int $DESIREDTIME
+	/usr/bin/sudo -u "$(logname)" defaults -currentHost write com.apple.screensaver idleTime -int "$DESIREDTIME"
 	echo "Screensaver: Set from $CURRENTTIME to $DESIREDTIME"
 	
 	# Restart the cfpresd to update the preference
