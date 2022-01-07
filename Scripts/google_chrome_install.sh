e7fec644f15ed198ddc6c9ba7507acab731ec7ec
Ali Al-Itejawi <ali@al-itejawi.com>
2022-01-07 11:53:10 +0100

    Chrome download and install script

--- Scripts/google_chrome_install.sh
+++ Scripts/google_chrome_install.sh
@@ -8,20 +8,7 @@
 #
 # DESCRIPTION
 #
-# Automatically download and install Google Chrome
-#
-####################################################################################################
-#
-# HISTORY
-#
-# Created by Caine Hörr on 2016-07-25
-# v1.1 - 2016-10-11 - Caine Hörr
-# Added -nobrowse flag to hdiutil attach /tmp/$VendorDMG command line arguments
-# Shout out to Chad Brewer (cbrewer) on JAMFNation for this fix/update
-# https://jamfnation.jamfsoftware.com/viewProfile.html?userID=1685
-#
-# v1.0 - 2016-07-25 - Caine Hörr
-# Google Chrome Installation script
+# Automatically download and install the latest version of Google Chrome
 
 # Vendor supplied DMG file
 VendorDMG="googlechrome.dmg"
@@ -44,3 +31,5 @@ hdiutil detach "$GoogleChromeDMG"
 
 # Remove the downloaded vendor supplied DMG file
 rm -f "/tmp/$VendorDMG"
+
+echo "Installation complete!"
